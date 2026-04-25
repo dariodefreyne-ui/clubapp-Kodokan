c70f164d8baf04ae9c5a59e924d586d0ede8c75c
dariodefreyne-ui <dario.de.freyne@gmail.com>
2026-04-25 14:03:15 +0200

    Update Trainingen.jsx

--- src/pages/Trainingen.jsx
+++ src/pages/Trainingen.jsx
@@ -637,7 +637,6 @@ function TrainingFormulier({ groepId, datum, trainingsData, technieken, onClose,
 
       for (let i = 0; i < technieksLijst.length; i++) {
         const t = technieksLijst[i];
-        if (!t.techniekNaam && !t.techniekId) continue;
         if (!t.techniekNaam && !t.techniekId&& !t.basisvaardigheud) continue;
         const data = {
           basisvaardigheid: t.basisvaardigheid || '',
@@ -1311,3 +1310,4 @@ export default function Trainingen() {
       )}
     </div>
   );
+}
