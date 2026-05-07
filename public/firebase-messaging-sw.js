ae02fddd73f7d45140674a315d4f4ee8739e27d0
dariodefreyne-ui <dario.de.freyne@gmail.com>
2026-05-07 19:11:51 +0200

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -36,5 +36,7 @@ messaging.onBackgroundMessage(payload => {
 
 self.addEventListener('notificationclick', event => {
   event.notification.close();
-  event.waitUntil(clients.openWindow('/winkel'));
+  const url = event.notification.data?.url || event.data?.url || '/';
+  event.waitUntil(clients.openWindow(url));
 });
+
