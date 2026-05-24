a7c9320ecac4054d0c36f23924eb8d2810fc8bbd
dariodefreyne-ui <dario.de.freyne@gmail.com>
2026-05-24 13:35:57 +0200

    Update ExcelUpload.jsx

--- src/components/trainingen/ExcelUpload.jsx
+++ src/components/trainingen/ExcelUpload.jsx
@@ -43,7 +43,7 @@ function parseDatumTijdzone(raw) {
 const JAPANSE_SYNONIEMEN = {
   'seoi': 'seo', 'seio': 'seo', 'shio': 'shiho',
   'katame': 'gatame', 'goruma': 'guruma', 'geruma': 'guruma',
-  'sasai': 'sasae', 'ippon seo': 'ippon seoi',
+  'sasai': 'sasae', 'ippon seo': 'ippon seoi','gesa':'kesa'
 };
 
 function normaliseerTechniek(s) {
